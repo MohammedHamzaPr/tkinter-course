f0752173489357d9b64260d84ab0cffb4804e254
Mohamed <60695273+MohammedHamzaPr@users.noreply.github.com>
2024-01-28 13:51:59 +0300

    Add files via upload

---
+++ Thrid Lesson.py
@@ -0,0 +1,8 @@
+from tkinter import Tk # احضار المكتبة التي نعمل عليها
+root = Tk() # انشاء نافذة
+root.geometry('500x500+450+100') # تغير مكان وحجم ضهور النافذة
+root.title('Tkinter With Mohammed') # اضافة عنوان للنافذة 
+root.configure(background='#262626') # تغير لون خلفية البرنامج
+
+
+root.mainloop() # تكرار عدد فريمات النافذة 
